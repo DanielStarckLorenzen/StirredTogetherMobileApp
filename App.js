d8327ee2729755a821fbe4e3624a576766f9b656
DanielStarckLorenzen <99916244+DanielStarckLorenzen@users.noreply.github.com>
2024-06-29 11:24:12 +0200

    haptic feedback 1.0.4

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
 
 export default function App() {
+  const webviewRef = React.useRef(null);
+
+  const onMessage = (event) => {
+    const message = event.nativeEvent.data;
+    if (message === 'triggerHapticFeedback') {
+      ReactNativeHapticFeedback.trigger("impactMedium", {
+        enableVibrateFallback: true,
+        ignoreAndroidSystemSettings: false
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.safeAreaTop} />
       <View style={styles.webViewContainer}>
         <WebView
+          ref={webviewRef}
           source={{ uri: 'https://stirredtogether.web.app' }}
           style={{ flex: 1 }}
           mediaPlaybackRequiresUserAction={false}
@@ -21,12 +35,13 @@ export default function App() {
           allowFileAccessFromFileURLs={true}
           allowUniversalAccessFromFileURLs={true}
           mixedContentMode="always"
+          onMessage={onMessage}
         />
       </View>
       <SafeAreaView style={styles.safeAreaBottom} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
